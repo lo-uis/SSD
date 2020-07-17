ce3e750d360d6732e83a4ca70059b7f974d65616
lo-uis <q108040r@mail.kyutech.jp>
2020-07-17 14:44:06 +0900

    1st commit

---
+++ dataset.py
@@ -0,0 +1,72 @@
+import os,path as ops
+import random
+
+import xml.etree.ElementTree as ET  #XMLファイルをテキストから読み込み、加工、保存
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.utils.data as data
+
+# 乱数のシードを設定????????
+torch.manual_seed(1234)
+np.random.seed(1234)
+random.seed(1234)
+
+# 学習、検証の画像データとアノテーションデータへのファイルパスリストを作成する
+
+
+def make_datapath_list(rootpath):
+    """
+    データへのパスを格納したリストを作成する。
+
+    Parameters
+    ----------
+    rootpath : str
+        データフォルダへのパス
+
+    Returns
+    -------
+    ret : train_img_list, train_anno_list, val_img_list, val_anno_list
+        データへのパスを格納したリスト
+    """
+
+    # 画像ファイルとアノテーションファイルへのパスのテンプレートを作成
+    imgpath_template = osp.join(rootpath, 'JPEGImages', '%s.jpg')
+    annopath_template = osp.join(rootpath, 'Annotations', '%s.xml')
+
+    # 訓練と検証、それぞれのファイルのID（ファイル名）を取得する
+    train_id_names = osp.join(rootpath + 'ImageSets/Main/train.txt')
+    val_id_names = osp.join(rootpath + 'ImageSets/Main/val.txt')
+
+    # 訓練データの画像ファイルとアノテーションファイルへのパスリストを作成
+    train_img_list = list()
+    train_anno_list = list()
+
+    for line in open(train_id_names):
+        file_id = line.strip()  # 空白スペースと改行を除去
+        img_path = (imgpath_template % file_id)  # 画像のパス
+        anno_path = (annopath_template % file_id)  # アノテーションのパス
+        train_img_list.append(img_path)  # リストに追加
+        train_anno_list.append(anno_path)  # リストに追加
+
+    # 検証データの画像ファイルとアノテーションファイルへのパスリストを作成
+    val_img_list = list()
+    val_anno_list = list()
+
+    for line in open(val_id_names):
+        file_id = line.strip()  # 空白スペースと改行を除去
+        img_path = (imgpath_template % file_id)  # 画像のパス
+        anno_path = (annopath_template % file_id)  # アノテーションのパス
+        val_img_list.append(img_path)  # リストに追加
+        val_anno_list.append(anno_path)  # リストに追加
+
+    return train_img_list, train_anno_list, val_img_list, val_anno_list
+
+# ファイルパスのリストを作成
+rootpath = "./data/VOCdevkit/VOC2012/"
+train_img_list, train_anno_list, val_img_list, val_anno_list = make_datapath_list(rootpath)
+
+# 動作確認
+print(train_img_list[0])
