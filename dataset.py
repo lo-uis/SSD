bb0ff615f32ce90a021fb13ab8e427ee071bbe2b
lo-uis <q108040r@mail.kyutech.jp>
2020-07-18 21:03:52 +0900

    3rd

--- dataset.py
+++ dataset.py
@@ -69,4 +69,103 @@ rootpath = "/home/kitahara/Desktop/VOCdevkit/VOC2012/"
 train_img_list, train_anno_list, val_img_list, val_anno_list = make_datapath_list(rootpath)
 
 # 動作確認
-print(train_img_list[0])
+#print(train_img_list[0])
+
+
+# 「XML形式のアノテーション」を、リスト形式に変換するクラス
+
+
+class Anno_xml2list(object):
+    """
+    1枚の画像に対する「XML形式のアノテーションデータ」を、画像サイズで規格化してからリスト形式に変換する。
+
+    Attributes
+    ----------
+    classes : リスト
+        VOCのクラス名を格納したリスト
+    """
+
+    def __init__(self, classes):
+
+        self.classes = classes
+
+    def __call__(self, xml_path, width, height):
+        """
+        1枚の画像に対する「XML形式のアノテーションデータ」を、画像サイズで規格化してからリスト形式に変換する。
+
+        Parameters
+        ----------
+        xml_path : str
+            xmlファイルへのパス。
+        width : int
+            対象画像の幅。
+        height : int
+            対象画像の高さ。
+
+        Returns
+        -------
+        ret : [[xmin, ymin, xmax, ymax, label_ind], ... ]
+            物体のアノテーションデータを格納したリスト。画像内に存在する物体数分のだけ要素を持つ。
+        """
+
+        # 画像内の全ての物体のアノテーションをこのリストに格納します
+        ret = []
+
+        # xmlファイルを読み込む
+        xml = ET.parse(xml_path).getroot()
+
+        # 画像内にある物体（object）の数だけループする
+        for obj in xml.iter('object'):
+
+            # アノテーションで検知がdifficultに設定されているものは除外
+            difficult = int(obj.find('difficult').text)
+            if difficult == 1:
+                continue
+
+            # 1つの物体に対するアノテーションを格納するリスト
+            bndbox = []
+
+            name = obj.find('name').text.lower().strip()  # 物体名
+            bbox = obj.find('bndbox')  # バウンディングボックスの情報
+
+            # アノテーションの xmin, ymin, xmax, ymaxを取得し、0～1に規格化
+            pts = ['xmin', 'ymin', 'xmax', 'ymax']
+
+            for pt in (pts):
+                # VOCは原点が(1,1)なので1を引き算して（0, 0）に
+                cur_pixel = int(bbox.find(pt).text) - 1
+
+                # 幅、高さで規格化
+                if pt == 'xmin' or pt == 'xmax':  # x方向のときは幅で割算
+                    cur_pixel /= width
+                else:  # y方向のときは高さで割算
+                    cur_pixel /= height
+
+                bndbox.append(cur_pixel)
+
+            # アノテーションのクラス名のindexを取得して追加
+            label_idx = self.classes.index(name)
+            bndbox.append(label_idx)
+
+            # resに[xmin, ymin, xmax, ymax, label_ind]を足す
+            ret += [bndbox]
+
+        return np.array(ret)  # [[xmin, ymin, xmax, ymax, label_ind], ... ]
+
+# 動作確認　
+voc_classes = ['aeroplane', 'bicycle', 'bird', 'boat',
+               'bottle', 'bus', 'car', 'cat', 'chair',
+               'cow', 'diningtable', 'dog', 'horse',
+               'motorbike', 'person', 'pottedplant',
+               'sheep', 'sofa', 'train', 'tvmonitor']
+
+transform_anno = Anno_xml2list(voc_classes)
+
+# 画像の読み込み OpenCVを使用
+ind = 1
+image_file_path = val_img_list[ind]
+img = cv2.imread(image_file_path)  # [高さ][幅][色BGR]
+height, width, channels = img.shape  # 画像のサイズを取得
+
+# アノテーションをリストで表示
+transform_anno(val_anno_list[ind], width, height)
